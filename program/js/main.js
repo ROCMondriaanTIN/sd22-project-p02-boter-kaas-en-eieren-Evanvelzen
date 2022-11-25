25eebb077717536836d5e6552e130bc2a096bc90
Evanvelzen <112620558+Evanvelzen@users.noreply.github.com>
2022-11-25 15:17:11 +0100

    startpage en begin javascript
    start page gemaakt en afgemakt (nog niet helemaal af (de ahref moet in het gehele div clickbaar zijn) begin javascript

---
+++ program/js/main.js
@@ -0,0 +1,15 @@
+console.log('main.js loaded');
+
+alert('welkom bij Boter, Kaas en Eieren');
+
+alert(`het doel is simpel.
+
+Probeer als eerste 3 op een rij te krijgen.`);
+
+alert('Speler 1 gebruikt cirkels');
+
+alert('Enspeler 2 gebruikt kruisjes');
+
+const player1 = prompt('Wat is de naam van speler 1?');
+
+const player2 = prompt('wat is de naam van speler 2?');
