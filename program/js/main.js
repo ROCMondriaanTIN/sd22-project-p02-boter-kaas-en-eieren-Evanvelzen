a34ddd4a376762bfdab008ce3962715d39dcbd48
Evanvelzen <112620558+Evanvelzen@users.noreply.github.com>
2022-11-29 10:26:32 +0100

    main js
    belangrijkste javascript afgemaakt

--- program/js/main.js
+++ program/js/main.js
@@ -6,10 +6,39 @@ alert(`het doel is simpel.
 
 Probeer als eerste 3 op een rij te krijgen.`);
 
-alert('Speler 1 gebruikt cirkels');
+alert('bij dit spel begint speler 1')
 
-alert('Enspeler 2 gebruikt kruisjes');
+alert('Speler 1 gebruikt  kruisjes');
 
-const player1 = prompt('Wat is de naam van speler 1?');
+alert('En speler 2 gebruikt cirkels');
 
-const player2 = prompt('wat is de naam van speler 2?');
+const player1name = prompt('Wat is de naam van speler 1?');
+
+confirm(`Weet u zeker dat uw naam: ${player1name} is?`);
+
+const player2name = prompt('wat is de naam van speler 2?');
+
+confirm(`Weet u zeker dat uw naam: ${player2name} is?`);
+
+const winstcombinatie = [[0, 1, 2], [3, 4, 5], [6, 7, 8][0, 3, 6], [1, 4, 7][2, 5, 8], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+
+const boxes = document.querySelectorAll('.box');
+
+let toggle = false;
+
+boxes.forEach(box => {
+    box.addEventListener('click', function (e) {
+        if (box.textContent != "") {
+            console.log("box.tile reageerd niet");
+            return
+        }
+        console.log('box.tile reageerd: ', e.target);
+        if (toggle) {
+            e.target.textContent = "O";
+        } else {
+            e.target.textContent = "X"
+        }
+
+        toggle = !toggle
+    });
+})
