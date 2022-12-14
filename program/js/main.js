bd37fc16d6458647a1b3f9f376d9cbc3d5fe0534
Evanvelzen <112620558+Evanvelzen@users.noreply.github.com>
2022-12-14 11:59:38 +0100

    fix bug te vroeg gelijk-spel
    bug gefixed dat ervoor zorgde dat het spel te vroeg beindigde

--- program/js/main.js
+++ program/js/main.js
@@ -1,33 +1,36 @@
-console.log('main.js loaded');
+// console.log('main.js loaded');
 
-alert('welkom bij Boter, Kaas en Eieren');
+// alert('welkom bij Boter, Kaas en Eieren');
 
-alert(`het doel is simpel.
+// alert(`het doel is simpel.
 
-Probeer als eerste 3 op een rij te krijgen.`);
+// Probeer als eerste 3 op een rij te krijgen.`);
 
-alert('bij dit spel begint speler 1')
+// alert('bij dit spel begint speler 1')
 
-alert('Speler 1 gebruikt  kruisjes');
+// alert('Speler 1 gebruikt  kruisjes');
 
-alert('En speler 2 gebruikt cirkels');
+// alert('En speler 2 gebruikt cirkels');
 
-const player1name = prompt('Wat is de naam van speler 1?');
+// const player1name = prompt('Wat is de naam van speler 1?');
 
-confirm(`Weet u zeker dat uw naam: ${player1name} is?`);
+// const player2name = prompt('wat is de naam van speler 2?');
 
-const player2name = prompt('wat is de naam van speler 2?');
+const winningcombination = [[0, 1, 2], [3, 4, 5], [6, 7, 8],[0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 
-confirm(`Weet u zeker dat uw naam: ${player2name} is?`);
 
-const winstcombinatie = [[0, 1, 2], [3, 4, 5], [6, 7, 8][0, 3, 6], [1, 4, 7][2, 5, 8], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 
 const boxes = document.querySelectorAll('.box');
 
+let stopping = false;
 let toggle = false;
 
 boxes.forEach(box => {
     box.addEventListener('click', function (e) {
+        if(stopping){
+            return;
+        } //zorgt ervoor dat je niet kan blijven clicken
+
         if (box.textContent != "") {
             console.log("box.tile reageerd niet");
             return
@@ -39,6 +42,39 @@ boxes.forEach(box => {
             e.target.textContent = "X"
         }
 
-        toggle = !toggle
+        toggle = !toggle;
+
+        checkForWin();
     });
 })
+
+  
+function checkForWin(){
+    for(let i = 0; i < winningcombination.length; i++) {
+      const row = winningcombination[i];
+
+        const position_a = row[0];
+        const position_b = row[1];
+        const position_c = row[2]; 
+
+        const box_a = boxes[position_a];
+        const box_b = boxes[position_b];
+        const box_c = boxes[position_c];
+
+
+        if(box_a.textContent == "" || box_b.textContent == "" || box_c.textContent == "") {
+            continue;
+        }
+
+
+        else if(box_a.textContent == box_b.textContent && box_b.textContent == box_c.textContent) {
+            console.log("Winnede situatie");
+            alert("gefeliciteerd");
+            
+            // window.location.href = "index.html";
+             stopping = true
+        } else{
+            console.log("gelijk spel");
+        }
+    }
+}
