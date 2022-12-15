5c66a302f797fadd270930f6a0501801f9f9c327
Evanvelzen <112620558+Evanvelzen@users.noreply.github.com>
2022-12-15 12:56:04 +0100

    alerts aangezet
    alerts aangezet

--- program/js/main.js
+++ program/js/main.js
@@ -1,20 +1,20 @@
-// console.log('main.js loaded');
+console.log('main.js loaded');
 
-// alert('welkom bij Boter, Kaas en Eieren');
+alert('welkom bij Boter, Kaas en Eieren');
 
-// alert(`het doel is simpel.
+alert(`het doel is simpel.
 
-// Probeer als eerste 3 op een rij te krijgen.`);
+Probeer als eerste 3 op een rij te krijgen.`);
 
-// alert('bij dit spel begint speler 1')
+alert('bij dit spel begint speler 1')
 
-// alert('Speler 1 gebruikt  kruisjes');
+alert('Speler 1 gebruikt  kruisjes');
 
-// alert('En speler 2 gebruikt cirkels');
+alert('En speler 2 gebruikt cirkels');
 
-// const player1name = prompt('Wat is de naam van speler 1?');
+const player1name = prompt('Wat is de naam van speler 1?');
 
-// const player2name = prompt('wat is de naam van speler 2?');
+const player2name = prompt('wat is de naam van speler 2?');
 
 const winningcombination = [[0, 1, 2], [3, 4, 5], [6, 7, 8],[0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 
@@ -74,7 +74,7 @@ function checkForWin(){
 
         else if(box_a.textContent == box_b.textContent && box_b.textContent == box_c.textContent) {
             console.log("Winnede situatie");
-            alert("gefeliciteerd");
+            alert("Er is een Winnaar");
             
             // window.location.href = "index.html";
              stopping = true
