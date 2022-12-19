7160124af277c17d73030d1ba2c4ad505e73bd5d
Evanvelzen <112620558+Evanvelzen@users.noreply.github.com>
2022-12-19 09:59:00 +0100

    full fix winfunction
    win condition voor spel werkt nu volledig, enkelt nog comments toe voegen & knop op index.html volledig clickbaar maken

--- program/js/main.js
+++ program/js/main.js
@@ -10,13 +10,13 @@ alert('bij dit spel begint speler 1')
 
 alert('Speler 1 gebruikt  kruisjes');
 
-alert('En speler 2 gebruikt cirkels');
+alert('speler 2 gebruikt cirkels');
 
 const player1name = prompt('Wat is de naam van speler 1?');
 
 const player2name = prompt('wat is de naam van speler 2?');
 
-const winningcombination = [[0, 1, 2], [3, 4, 5], [6, 7, 8],[0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+const winningcombination = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 
 
 
@@ -26,26 +26,29 @@ const info = document.querySelector('.info');
 let stopping = false;
 let toggle = false;
 
+info.innerHTML = `beurt: ${player1name}`
+
 boxes.forEach(box => {
     box.addEventListener('click', function (e) {
-        if(stopping){
+        if (stopping) {
             return;
         } //zorgt ervoor dat je niet kan blijven clicken
 
         if (box.textContent != "") {
             console.log("box.tile reageerd niet");
-    
+
             return
         }
         console.log('box.tile reageerd: ', e.target);
         if (toggle) {
             e.target.textContent = "O";
-            info.innerHTML=('beurt: speler 1')
+            info.innerHTML = (`beurt: ${player1name}`)
 
         } else {
             e.target.textContent = "X";
-            info.innerHTML=('beurt: speler 2')
-;        }
+            info.innerHTML = (`beurt: ${player2name}`)
+                ;
+        }
 
         toggle = !toggle;
 
@@ -53,33 +56,36 @@ boxes.forEach(box => {
     });
 })
 
-  
-function checkForWin(){
-    for(let i = 0; i < winningcombination.length; i++) {
-      const row = winningcombination[i];
+
+function checkForWin() {
+    for (let i = 0; i < winningcombination.length; i++) {
+        const row = winningcombination[i];
 
         const position_a = row[0];
         const position_b = row[1];
-        const position_c = row[2]; 
+        const position_c = row[2];
 
         const box_a = boxes[position_a];
         const box_b = boxes[position_b];
         const box_c = boxes[position_c];
 
 
-        if(box_a.textContent == "" || box_b.textContent == "" || box_c.textContent == "") {
+        if (box_a.textContent == "" || box_b.textContent == "" || box_c.textContent == "") {
             continue;
         }
 
-
-        else if(box_a.textContent == box_b.textContent && box_b.textContent == box_c.textContent) {
-            console.log("Winnede situatie");
-            alert("Er is een Winnaar");
-            
-            // window.location.href = "index.html";
-             stopping = true
-        } else{
-            console.log("gelijk spel");
+        else if (box_a.textContent == box_b.textContent && box_b.textContent == box_c.textContent && box_c.textContent == "X") {
+            console.log(`Winnende Situatie`);
+            alert(`Spel gewonnen door ${player1name}`);
+            window.location.href = "index.html"
+            stopping = true;
+        } 
+        
+        else if(box_a.textContent == box_b.textContent && box_b.textContent == box_c.textContent && box_c.textContent == "O") {
+            console.log(`Winnende Situatie`);
+            alert(`Spel gewonnen door ${player2name}`);
+            window.location.href = "index.html"
+            stopping = true;
         }
     }
 }
